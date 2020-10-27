749e596c97e3c1389ac1699abf2d38f03d773497
Brian Kubes <briankubes@gmail.com>
2020-10-27 13:10:41 -0700

    get is working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,24 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const Posts = require('./posts-model')
 
 // const router = require('express').Router()
 
 // @desc		Get all posts
 // @route		GET /
-router.get('/', (req, res) => {
-
+router.get('/api/posts', (req, res) =>{
+    Posts.find(req.query)
+    .then(posts => {
+        console.log(posts)
+        res.status(200).json(posts)
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: error.message, 
+            stack: error.stack
+        })
+    })
 })
-
-
 // @desc		Get a post by id
 // @route		GET /:id
-
+router.get('/:id')
+// @desc		Get a post by id
+// @route		GET /:id/comments
+router.get('/api/posts/:id/comments')
  // @desc		Add a new post
 // @route		POST /
-
+// @desc		Add a new post
+// @route		POST /:id/comments
+router.post('/api/posts/:id/comments')
 // @desc		Update a post by id
- // @route		PATCH /:id
-
+ // @route		PUT /:id
 // @desc		Remove a post by id
 // @route		DELETE /:id
 
