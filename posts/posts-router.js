c7459bc986ccd4652d3a42ba31de78b3df97a611
Brian Kubes <briankubes@gmail.com>
2020-10-27 12:51:11 -0700

    setup

---
+++ posts/posts-router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+// const router = require('express').Router()
+
+// @desc		Get all posts
+// @route		GET /
+router.get('/', (req, res) => {
+
+})
+
+
+// @desc		Get a post by id
+// @route		GET /:id
+
+ // @desc		Add a new post
+// @route		POST /
+
+// @desc		Update a post by id
+ // @route		PATCH /:id
+
+// @desc		Remove a post by id
+// @route		DELETE /:id
+
+
+module.exports = router
